562da9494c250404ce88d355dff76d89fc7ef270
sopiya <sopiya@gmail.com>
2025-02-10 17:38:23 +0800

    Feat: User roles display in user management page

--- Hometown_Application/Controllers/DashboardController.cs
+++ Hometown_Application/Controllers/DashboardController.cs
@@ -4,17 +4,21 @@ using Microsoft.AspNetCore.Mvc;
 using Hometown_Application.Areas.Identity.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 
 namespace Hometown_Application.Controllers
 {
-    [Authorize] 
+    [Authorize]
     public class DashboardController : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
-        public DashboardController(UserManager<ApplicationUser> userManager)
+        public DashboardController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
         {
             _userManager = userManager;
+            _roleManager = roleManager;
         }
 
         public IActionResult Index()
@@ -25,7 +29,14 @@ namespace Hometown_Application.Controllers
         [Authorize(Roles = "Admin")]
         public async Task<IActionResult> UserManagement()
         {
-            var users = _userManager.Users.ToList(); 
+            var users = await _userManager.Users.ToListAsync(); // Fetch all users
+
+            foreach (var user in users)
+            {
+                var roles = await _userManager.GetRolesAsync(user); // Fetch roles for each user
+                user.PhoneNumber = roles.FirstOrDefault() ?? "No Role"; // Store role in an unused field (e.g., PhoneNumber)
+            }
+
             return View(users);
         }
     }
